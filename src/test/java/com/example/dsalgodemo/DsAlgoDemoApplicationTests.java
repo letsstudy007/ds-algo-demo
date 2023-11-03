6c3895851fa0fae269ea5d0922d95b0e8e196406
gyandeep <gyandeepsharma007@gmail.com>
2023-11-03 20:49:42 +0530

    committing simple spring boot application with no code

---
+++ src/test/java/com/example/dsalgodemo/DsAlgoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsalgodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAlgoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
